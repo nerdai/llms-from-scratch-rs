2923849534f3e36c328aa7bc652cc0b3b2f24b1c
nerdai <andrei@nerdai.io>
2024-10-29 10:33:45 -0400

    impl encode

--- src/listings/ch2.rs
+++ src/listings/ch2.rs
@@ -1,3 +1,4 @@
+use fancy_regex::Regex;
 use std::collections::HashMap;
 
 // Listing 2.3
@@ -23,6 +24,20 @@ impl SimpleTokenizerV1 {
     pub fn int_to_str(&self) -> &HashMap<i32, String> {
         &self.int_to_str
     }
+
+    pub fn encode(&self, text: &str) -> Vec<i32> {
+        let re = Regex::new(r#"([,.?_!"()']|--|\s)"#).unwrap();
+        let preprocessed: Vec<&str> = re.split(text).map(|x| x.unwrap()).collect();
+        preprocessed
+            .into_iter()
+            .map(|s| self.str_to_int.get(&String::from(s)).unwrap())
+            .cloned()
+            .collect()
+    }
+
+    pub fn decode(self, _ids: Vec<i32>) -> String {
+        todo!()
+    }
 }
 
 #[cfg(test)]
@@ -46,5 +61,22 @@ mod tests {
         assert_eq!(tokenizer.str_to_int.get(&String::from("is")), Some(&2));
         assert_eq!(tokenizer.str_to_int.get(&String::from("a")), Some(&3));
         assert_eq!(tokenizer.str_to_int.get(&String::from("test")), Some(&4));
+    }
+
+    #[test]
+    fn test_encode() {
+        let mut vocab: HashMap<&str, i32> = HashMap::new();
+        vocab.entry("this").or_insert(1);
+        vocab.entry("is").or_insert(2);
+        vocab.entry("a").or_insert(3);
+        vocab.entry("test").or_insert(4);
+        let tokenizer = SimpleTokenizerV1::from_vocab(vocab);
+
+        let token_ids = tokenizer.encode("this is a test");
+
+        assert_eq!(token_ids[0], 1);
+        assert_eq!(token_ids[1], 2);
+        assert_eq!(token_ids[2], 3);
+        assert_eq!(token_ids[3], 4);
     }
 }
