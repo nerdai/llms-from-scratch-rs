f6d9a566353a6d912c0799aa415414158a52115a
nerdai <andrei@nerdai.io>
2024-10-29 23:39:39 -0400

    add scaffolding for exercises

---
+++ src/exercises/ch2/mod.rs
@@ -0,0 +1,27 @@
+use crate::Exercise;
+
+/// 2.1
+pub struct X2P1 {}
+
+impl Exercise for X2P1 {
+    fn name(&self) -> String {
+        String::from("2.1")
+    }
+
+    fn main(&self) {
+        todo!()
+    }
+}
+
+/// 2.2
+pub struct X2P2 {}
+
+impl Exercise for X2P2 {
+    fn name(&self) -> String {
+        String::from("2.2")
+    }
+
+    fn main(&self) {
+        todo!()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
+pub mod exercises;
 pub mod listings;
+
+pub trait Exercise {
+    fn name(&self) -> String;
+
+    fn main(&self);
+}
