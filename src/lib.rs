928e480b8c93cbdd671ff4038a5b78f63c8694ff
nerdai <andrei@nerdai.io>
2024-11-06 00:22:26 -0500

    add examples module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod examples;
 pub mod exercises;
 pub mod listings;
 
@@ -6,4 +7,33 @@ pub trait Exercise: Send + Sync {
     fn name(&self) -> String;
 
     fn main(&self);
+}
+
+pub struct Example {
+    description: String,
+    source_page: usize,
+}
+
+impl Example {
+    pub fn new(desc: &str, source_page: usize) -> Self {
+        Self {
+            description: String::from(desc),
+            source_page,
+        }
+    }
+
+    pub fn description(&self) -> &str {
+        &self.description[..]
+    }
+
+    pub fn source_page(&self) -> usize {
+        self.source_page
+    }
+}
+
+impl Main for Example {}
+pub trait Main: Send + Sync {
+    fn main(&self) {
+        println!("The main method has not been implemented yet.")
+    }
 }
