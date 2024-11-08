0014b625b4876ff45263bb7663fb4348c29d5477
nerdai <andrei@nerdai.io>
2024-11-08 00:10:13 -0500

    add softmax attn scores

--- src/examples/ch03.rs
+++ src/examples/ch03.rs
@@ -20,7 +20,7 @@ impl Example for EG01 {
             &[
                 [0.43_f32, 0.15, 0.89], // Your
                 [0.55, 0.87, 0.66],     // journey
-                [0.57, 0.85, 0.67],     // starts
+                [0.57, 0.85, 0.64],     // starts
                 [0.22, 0.58, 0.33],     // with
                 [0.77, 0.25, 0.10],     // one
                 [0.05, 0.80, 0.55],     // step
@@ -51,12 +51,23 @@ impl Example for EG01 {
         }
 
         if let Some(attn_scores_2) = optional_attn_scores_2 {
+            // raw attention scores
+            println!("Raw attention scores: {:?}", attn_scores_2);
+
+            // basic normalization
             let sum = attn_scores_2.sum_all().unwrap();
+            let normalized_attn_scores = (attn_scores_2.broadcast_div(&sum))
+                .unwrap()
+                .to_vec1::<f32>();
+            println!("Normalized attention scores: {:?}", normalized_attn_scores);
+
+            // softmax normalization
+            let exponentiator = Tensor::exp(&attn_scores_2).unwrap();
+            let exponentiator_sum = exponentiator.sum_all().unwrap();
+            let softmax_attn_scores = exponentiator.broadcast_div(&exponentiator_sum).unwrap();
             println!(
-                "{:?}",
-                (attn_scores_2.broadcast_div(&sum))
-                    .unwrap()
-                    .to_vec1::<f32>()
+                "Softmax-normalized attention scores: {:?}",
+                softmax_attn_scores
             );
         }
     }
