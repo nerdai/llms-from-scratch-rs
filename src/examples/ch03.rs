d02043ab2ce87841eb468a514fb986ce5ccea6ea
nerdai <andrei@nerdai.io>
2024-11-09 10:01:07 -0500

    dry for inptus

--- src/examples/ch03.rs
+++ src/examples/ch03.rs
@@ -1,4 +1,20 @@
 use crate::Example;
+use candle_core::{Device, Tensor};
+
+fn get_inputs() -> Tensor {
+    Tensor::new(
+        &[
+            [0.43_f32, 0.15, 0.89], // Your
+            [0.55, 0.87, 0.66],     // journey
+            [0.57, 0.85, 0.64],     // starts
+            [0.22, 0.58, 0.33],     // with
+            [0.77, 0.25, 0.10],     // one
+            [0.05, 0.80, 0.55],     // step
+        ],
+        &Device::Cpu,
+    )
+    .unwrap()
+}
 
 /// Example 02.01
 pub struct EG01 {}
@@ -13,22 +29,11 @@ impl Example for EG01 {
     }
 
     fn main(&self) {
-        use candle_core::{Device, IndexOp, Tensor};
+        use candle_core::{IndexOp, Tensor};
         use candle_nn::ops::softmax;
 
-        let dev = Device::cuda_if_available(0).unwrap();
-        let inputs = Tensor::new(
-            &[
-                [0.43_f32, 0.15, 0.89], // Your
-                [0.55, 0.87, 0.66],     // journey
-                [0.57, 0.85, 0.64],     // starts
-                [0.22, 0.58, 0.33],     // with
-                [0.77, 0.25, 0.10],     // one
-                [0.05, 0.80, 0.55],     // step
-            ],
-            &dev,
-        )
-        .unwrap();
+        let inputs = get_inputs();
+        let dev = inputs.device().to_owned();
 
         let query = inputs
             .index_select(&Tensor::new(&[1u32], &dev).unwrap(), 0)
@@ -109,22 +114,9 @@ impl Example for EG02 {
     }
 
     fn main(&self) {
-        use candle_core::{Device, Tensor};
         use candle_nn::ops::softmax;
 
-        let dev = Device::cuda_if_available(0).unwrap();
-        let inputs = Tensor::new(
-            &[
-                [0.43_f32, 0.15, 0.89], // Your
-                [0.55, 0.87, 0.66],     // journey
-                [0.57, 0.85, 0.64],     // starts
-                [0.22, 0.58, 0.33],     // with
-                [0.77, 0.25, 0.10],     // one
-                [0.05, 0.80, 0.55],     // step
-            ],
-            &dev,
-        )
-        .unwrap();
+        let inputs = get_inputs();
 
         // matmul to get attn scores
         let attn_scores = inputs.matmul(&inputs.t().unwrap()).unwrap();
