7871e885a42088010e5ce8c621ded9a72682fa39
nerdai <andrei@nerdai.io>
2024-11-09 18:21:34 -0500

    cr

--- src/listings/ch03.rs
+++ src/listings/ch03.rs
@@ -1,8 +1,2 @@
-use candle_core::{Device, Result, Tensor};
-use fancy_regex::{Captures, Regex};
-use rand::{seq::SliceRandom, thread_rng};
-use std::collections::HashMap;
-use tiktoken_rs::CoreBPE;
-
 /// Listing 3.1
 pub struct SelfAttentionV1 {}
