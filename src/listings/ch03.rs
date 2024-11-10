5c545aad5fd282f1d13e10061abcccc0d5920c31
nerdai <andrei@nerdai.io>
2024-11-09 19:08:21 -0500

    impl constructor and getters

--- src/listings/ch03.rs
+++ src/listings/ch03.rs
@@ -1,2 +1,41 @@
+use candle_core::{Module, Result, Tensor};
+use candle_nn::VarBuilder;
+
 /// Listing 3.1
-pub struct SelfAttentionV1 {}
+pub struct SelfAttentionV1 {
+    w_query: Tensor,
+    w_key: Tensor,
+    w_value: Tensor,
+}
+
+impl SelfAttentionV1 {
+    pub fn new(d_in: usize, d_out: usize, vb: VarBuilder<'_>) -> Result<Self> {
+        let init = candle_nn::init::DEFAULT_KAIMING_NORMAL;
+        let w_query = vb.get_with_hints((d_in, d_out), "query", init)?;
+        let w_key = vb.get_with_hints((d_in, d_out), "key", init)?;
+        let w_value = vb.get_with_hints((d_in, d_out), "value", init)?;
+        Ok(Self {
+            w_query,
+            w_key,
+            w_value,
+        })
+    }
+
+    pub fn w_query(&self) -> &Tensor {
+        &self.w_query
+    }
+
+    pub fn w_key(&self) -> &Tensor {
+        &self.w_key
+    }
+
+    pub fn w_value(&self) -> &Tensor {
+        &self.w_value
+    }
+}
+
+impl Module for SelfAttentionV1 {
+    fn forward(&self, _xs: &Tensor) -> Result<Tensor> {
+        todo!()
+    }
+}
