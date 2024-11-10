f7037c5102a6e83a5397b747869f092cf14aba14
nerdai <andrei@nerdai.io>
2024-11-09 19:15:34 -0500

    add unit test

--- src/listings/ch03.rs
+++ src/listings/ch03.rs
@@ -39,3 +39,23 @@ impl Module for SelfAttentionV1 {
         todo!()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use candle_core::{DType, Device};
+    use candle_nn::{VarBuilder, VarMap};
+    use rstest::*;
+
+    #[rstest]
+    fn test_self_attention_v1_init() {
+        let (d_in, d_out) = (3_usize, 5_usize);
+        let varmap = VarMap::new();
+        let vb = VarBuilder::from_varmap(&varmap, DType::F32, &Device::Cpu);
+        let attn_v1_layer = SelfAttentionV1::new(d_in, d_out, vb.pp("attn")).unwrap();
+
+        assert_eq!(attn_v1_layer.w_query.dims(), &[d_in, d_out]);
+        assert_eq!(attn_v1_layer.w_key.dims(), &[d_in, d_out]);
+        assert_eq!(attn_v1_layer.w_value.dims(), &[d_in, d_out]);
+    }
+}
