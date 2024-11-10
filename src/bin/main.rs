fdc7a1cf0550883bd9c96c8119d1d92ef4801d05
nerdai <andrei@nerdai.io>
2024-11-10 16:24:51 -0500

    register EG 03.05; simplify module imports

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,24 @@
-use llms_from_scratch_rs::examples::{ch02, ch03};
-use llms_from_scratch_rs::exercises::ch02::{X2P1, X2P2};
-use llms_from_scratch_rs::{Example, Exercise};
+use llms_from_scratch_rs::{examples, exercises, Example, Exercise};
 use std::collections::HashMap;
 use std::sync::LazyLock;
 
 static EXERCISE_REGISTRY: LazyLock<HashMap<&'static str, Box<dyn Exercise>>> =
     LazyLock::new(|| {
         let mut m: HashMap<&'static str, Box<dyn Exercise + 'static>> = HashMap::new();
-        m.insert("2.1", Box::new(X2P1));
-        m.insert("2.2", Box::new(X2P2));
+        m.insert("2.1", Box::new(exercises::ch02::X2P1));
+        m.insert("2.2", Box::new(exercises::ch02::X2P2));
         m
     });
 
 static EXAMPLE_REGISTRY: LazyLock<HashMap<&'static str, Box<dyn Example>>> = LazyLock::new(|| {
     let mut m: HashMap<&'static str, Box<dyn Example + 'static>> = HashMap::new();
-    m.insert("02.01", Box::new(ch02::EG01));
-    m.insert("02.02", Box::new(ch02::EG02));
-    m.insert("03.01", Box::new(ch03::EG01));
-    m.insert("03.02", Box::new(ch03::EG02));
-    m.insert("03.03", Box::new(ch03::EG03));
-    m.insert("03.04", Box::new(ch03::EG04));
+    m.insert("02.01", Box::new(examples::ch02::EG01));
+    m.insert("02.02", Box::new(examples::ch02::EG02));
+    m.insert("03.01", Box::new(examples::ch03::EG01));
+    m.insert("03.02", Box::new(examples::ch03::EG02));
+    m.insert("03.03", Box::new(examples::ch03::EG03));
+    m.insert("03.04", Box::new(examples::ch03::EG04));
+    m.insert("03.05", Box::new(examples::ch03::EG05));
     m
 });
 
