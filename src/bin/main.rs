c19b1cca49071781deff3e84b94d0c9aadcf5a29
nerdai <andrei@nerdai.io>
2024-11-15 01:25:59 -0500

    add ex 3.2; re-jig main binary

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,6 +10,7 @@ static EXERCISE_REGISTRY: LazyLock<HashMap<&'static str, Box<dyn Exercise>>> =
         m.insert("2.2", Box::new(exercises::ch02::X2P2));
         // ch03
         m.insert("3.1", Box::new(exercises::ch03::X3P1));
+        m.insert("3.2", Box::new(exercises::ch03::X3P2));
         m
     });
 
@@ -32,11 +33,26 @@ static EXAMPLE_REGISTRY: LazyLock<HashMap<&'static str, Box<dyn Example>>> = Laz
     m
 });
 
+#[allow(dead_code)]
+enum RunType {
+    EG(String),
+    EX(String),
+}
+
 fn main() {
     let exercise_registry = &*EXERCISE_REGISTRY;
     let example_registry = &*EXAMPLE_REGISTRY;
-    let _ex = exercise_registry.get("3.1").unwrap();
-    let eg = example_registry.get("03.10").unwrap();
-    // ex.main()
-    eg.main()
+
+    let run_type = RunType::EX(String::from("3.2"));
+    // let run_type = RunType::EG(String::from("03.10"));
+    match run_type {
+        RunType::EX(id) => {
+            let ex = exercise_registry.get(&id[..]).unwrap();
+            ex.main()
+        }
+        RunType::EG(id) => {
+            let eg = example_registry.get(&id[..]).unwrap();
+            eg.main()
+        }
+    }
 }
--- src/exercises/ch03.rs
+++ src/exercises/ch03.rs
@@ -15,7 +15,8 @@ impl Exercise for X3P1 {
 
         let (d_in, d_out) = (3_usize, 5_usize);
         let varmap = VarMap::new();
-        let vb = VarBuilder::from_varmap(&varmap, DType::F32, &Device::Cpu);
+        let dev = Device::cuda_if_available(0).unwrap();
+        let vb = VarBuilder::from_varmap(&varmap, DType::F32, &dev);
         let attn_v2_layer = SelfAttentionV2::new(d_in, d_out, false, vb.pp("attn_v2")).unwrap();
         let attn_v1_layer = SelfAttentionV1 {
             w_query: attn_v2_layer.w_query().weight().t().unwrap(),
@@ -25,7 +26,7 @@ impl Exercise for X3P1 {
         };
 
         let input_length = 10_usize;
-        let xs = Tensor::rand(0f32, 1f32, (input_length, d_in), &Device::Cpu).unwrap();
+        let xs = Tensor::rand(0f32, 1f32, (input_length, d_in), &dev).unwrap();
         let context_vectors_from_v1 = attn_v1_layer.forward(&xs).unwrap();
         let context_vectors_from_v2 = attn_v2_layer.forward(&xs).unwrap();
 
@@ -34,5 +35,40 @@ impl Exercise for X3P1 {
             context_vectors_from_v1.to_vec2::<f32>().unwrap()
                 == context_vectors_from_v2.to_vec2::<f32>().unwrap()
         )
+    }
+}
+
+/// Exercise 3.2
+pub struct X3P2;
+
+impl Exercise for X3P2 {
+    fn name(&self) -> String {
+        String::from("3.2")
+    }
+
+    fn main(&self) {
+        use crate::listings::ch03::MultiHeadAttentionWrapper;
+        use candle_core::{DType, Device, Module, Tensor};
+        use candle_nn::{VarBuilder, VarMap};
+
+        let (d_in, d_out) = (3_usize, 1_usize); // set d_out to 1 to get desired final dim
+        let varmap = VarMap::new();
+        let dev = Device::cuda_if_available(0).unwrap();
+        let vb = VarBuilder::from_varmap(&varmap, DType::F32, &dev);
+        let num_heads = 2_usize;
+        let mha =
+            MultiHeadAttentionWrapper::new(num_heads, d_in, d_out, 0.0_f32, false, vb.pp("mha"))
+                .unwrap();
+
+        // create random input batch
+        let input_length = 6_usize;
+        let xs = Tensor::rand(0f32, 1f32, (input_length, d_in), &vb.device()).unwrap();
+        let batch = Tensor::stack(&[&xs, &xs], 0).unwrap();
+        println!("batch shape: {:?}", batch);
+
+        // run forward on mha
+        let context_vectors = mha.forward(&batch).unwrap();
+        println!("context_vectors.shape: {:?}", context_vectors);
+        println!("context_vectors: {:?}", context_vectors.to_vec3::<f32>());
     }
 }
