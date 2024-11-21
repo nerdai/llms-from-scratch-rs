937f021e7dfddbeacbf00d7f16e430a5704ab320
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2024-11-21 00:29:16 -0500

    Listing 4.7 (GPT2) (#47)
    * listing 4.7
    
    * nit

--- src/listings/ch04.rs
+++ src/listings/ch04.rs
@@ -47,7 +47,6 @@ impl Config {
 
 /// Listing 4.1
 /// DummyGPTModel
-#[allow(dead_code)]
 pub struct DummyGPTModel {
     tok_emb: Embedding,
     pos_emb: Embedding,
@@ -307,6 +306,57 @@ impl Module for TransformerBlock {
     }
 }
 
+/// Listing 4.7
+/// GPTModel
+pub struct GPTModel {
+    tok_emb: Embedding,
+    pos_emb: Embedding,
+    drop_emb: Dropout,
+    trf_blocks: Sequential, // of transformer blocks
+    final_norm: LayerNorm,
+    out_head: Linear,
+}
+
+impl GPTModel {
+    pub fn new(cfg: Config, vb: VarBuilder<'_>) -> Result<Self> {
+        let tok_emb = embedding(cfg.vocab_size, cfg.emb_dim, vb.pp("tok_emb"))?;
+        let pos_emb = embedding(cfg.context_length, cfg.emb_dim, vb.pp("pos_emb"))?;
+        let drop_emb = Dropout::new(cfg.drop_rate);
+        let mut trf_blocks = seq();
+        for ix in 0..cfg.n_layers {
+            trf_blocks =
+                trf_blocks.add(TransformerBlock::new(cfg, vb.pp(format!("trf-{}", ix))).unwrap());
+        }
+        let final_norm = LayerNorm::new(cfg.emb_dim, vb.pp("final_norm"))?;
+        let out_head = linear_b(cfg.emb_dim, cfg.vocab_size, false, vb.pp("out_head"))?;
+        Ok(Self {
+            tok_emb,
+            pos_emb,
+            drop_emb,
+            trf_blocks,
+            final_norm,
+            out_head,
+        })
+    }
+}
+
+impl Module for GPTModel {
+    fn forward(&self, xs: &Tensor) -> Result<Tensor> {
+        let (_batch_size, seq_len) = xs.dims2()?;
+        let tok_embeds = self.tok_emb.forward(xs)?;
+        let pos_ids = Tensor::arange(0u32, seq_len as u32, xs.device())?;
+        let pos_embeds = self.pos_emb.embeddings().index_select(&pos_ids, 0)?;
+
+        let mut x = tok_embeds.broadcast_add(&pos_embeds)?;
+        x = self.drop_emb.forward(&x, true)?;
+        x = self.trf_blocks.forward(&x)?;
+        x = self.final_norm.forward(&x)?;
+
+        let logits = self.out_head.forward(&x)?;
+        Ok(logits)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -515,5 +565,31 @@ mod tests {
 
         let out = transformer_block.forward(&batch_example).unwrap();
         assert_eq!(out.dims(), batch_example.dims());
+    }
+
+    #[rstest]
+    fn test_gpt_model_init(vb: VarBuilder<'_>) {
+        let cfg = Config::gpt_sm_test();
+        let model = GPTModel::new(cfg, vb).unwrap();
+
+        assert_eq!(model.pos_emb.hidden_size(), cfg.emb_dim);
+        assert_eq!(model.tok_emb.hidden_size(), cfg.emb_dim);
+        assert_eq!(model.trf_blocks.len() as usize, cfg.n_layers);
+        assert_eq!(
+            model.out_head.weight().dims(),
+            &[cfg.vocab_size, cfg.emb_dim]
+        );
+    }
+
+    #[rstest]
+    fn test_gpt_model_forward(vb: VarBuilder<'_>, batch_token_ids: Tensor) {
+        let (batch_size, seq_len) = batch_token_ids.dims2().unwrap();
+
+        let cfg = Config::gpt_sm_test();
+        let model = GPTModel::new(cfg, vb).unwrap();
+
+        let logits = model.forward(&batch_token_ids).unwrap();
+
+        assert_eq!(logits.dims(), &[batch_size, seq_len, cfg.vocab_size]);
     }
 }
