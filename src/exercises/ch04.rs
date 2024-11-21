12b4379fb7a2861773fd97c02af4e22935f37143
nerdai <andrei@nerdai.io>
2024-11-21 10:29:07 -0500

    start ch04 excercise module

---
+++ src/exercises/ch04.rs
@@ -0,0 +1,13 @@
+use crate::Exercise;
+
+pub struct X4P1;
+
+impl Exercise for X4P1 {
+    fn name(&self) -> String {
+        String::from("4.1")
+    }
+
+    fn main(&self) {
+        todo!()
+    }
+}
