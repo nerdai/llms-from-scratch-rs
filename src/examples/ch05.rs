251ad39bdc742bd89882ffda7047c4a043160264
nerdai <andrei@nerdai.io>
2024-11-24 16:51:53 -0500

    EG 05.01

--- src/examples/ch05.rs
+++ src/examples/ch05.rs
@@ -13,8 +13,37 @@ impl Example for EG01 {
 
     fn main(&self) {
         use crate::listings::{
-            ch04::generate_text_simple,
+            ch04::{generate_text_simple, Config, GPTModel},
             ch05::{text_to_token_ids, token_ids_to_text},
         };
+        use candle_core::{DType, Device};
+        use candle_nn::{VarBuilder, VarMap};
+        use tiktoken_rs::get_bpe_from_model;
+
+        // construct model
+        let varmap = VarMap::new();
+        let vb =
+            VarBuilder::from_varmap(&varmap, DType::F32, &Device::cuda_if_available(0).unwrap());
+        let cfg = Config::gpt2_124m();
+        let model = GPTModel::new(Config::gpt2_124m(), vb.pp("model")).unwrap();
+
+        // sample setup and load tokenizer
+        let start_context = "Every effort moves you";
+        let tokenizer = get_bpe_from_model("gpt2").unwrap();
+
+        // generate next tokens with model
+        let max_new_tokens = 10_usize;
+        let token_ids = generate_text_simple(
+            model,
+            text_to_token_ids(start_context, &tokenizer, vb.device()).unwrap(),
+            max_new_tokens,
+            cfg.context_length,
+        )
+        .unwrap();
+
+        println!(
+            "Output text:\n{:?}",
+            token_ids_to_text(token_ids, &tokenizer)
+        )
     }
 }
