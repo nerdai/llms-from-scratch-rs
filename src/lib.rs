d22bfdfc6d0aa5352721453f9145eb84029a4a78
nerdai <andrei@nerdai.io>
2024-12-06 20:36:54 -0500

    start module docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//! Build A Large Language Model From Scratch — Rust Translations
+//!
+//! ## Intro
+//!
+//! This crate provides Rust translations of the examples, exercises and listings
+//! found in the book Build A LLM From Scratch by Sebastian Raschka, which is
+//! a great resource for careful learning of LLMs. The book provides several
+//! examples and listings which are written in PyTorch in order to learn how to
+//! build a GPT (decoder-only) language model. This crate provides the Rust
+//! equivalent for nearly all of the code provided in the book using Candle
+//! (a Minimalist ML framework for Rust).
+//!
+//!
+
 use anyhow::Result;
 
 pub mod candle_addons;
