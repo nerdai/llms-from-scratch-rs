f50f2fdc589b306313a2cdcab2f3db7432569af1
nerdai <andrei@nerdai.io>
2024-12-16 22:30:08 -0500

    init listing 6.1

---
+++ src/listings/ch06.rs
@@ -0,0 +1,13 @@
+//! Listings from Chapter 6
+
+use candle_core::Result;
+
+/// [Listing 6.1] Downloading and unzipping the dataset
+pub fn download_and_unzip_spam_data(
+    url: &str,
+    zip_path: &str,
+    extracted_path: &str,
+    datafile_path: &str,
+) -> Result<()> {
+    todo!()
+}
