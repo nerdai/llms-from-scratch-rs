25d068f2bae23b026a5d3b547a9e1d777110b55d
nerdai <andrei@nerdai.io>
2024-12-16 22:39:19 -0500

    add reqwest

--- src/listings/ch06.rs
+++ src/listings/ch06.rs
@@ -2,6 +2,12 @@
 
 use candle_core::Result;
 
+pub const URL: &'static str =
+    "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip";
+pub const ZIP_PATH: &'static str = "sms_spam_collection.zip";
+pub const EXTRACTED_PATH: &'static str = "sms_spam_collection";
+pub const DATA_FILE_PATH: &'static str = "sms_spam_collection/SMSSPamCollection.tsv";
+
 /// [Listing 6.1] Downloading and unzipping the dataset
 pub fn download_and_unzip_spam_data(
     url: &str,
