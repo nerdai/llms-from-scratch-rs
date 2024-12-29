e7d5d6ee8f43e6fb19e1aa5f134b174a983399f5
nerdai <andrei@nerdai.io>
2024-12-29 18:25:25 -0500

    wip

--- src/listings/ch06.rs
+++ src/listings/ch06.rs
@@ -12,7 +12,7 @@ use candle_datasets::{batcher::IterResult2, Batcher};
 use candle_nn::{linear_b, Optimizer, VarBuilder, VarMap};
 use hf_hub::api::sync::Api;
 use plotly::common::Mode;
-use plotly::{Plot, Scatter};
+use plotly::{Layout, Plot, Scatter};
 use polars::prelude::*;
 use rand::{seq::SliceRandom, thread_rng};
 use std::cmp;
@@ -907,18 +907,18 @@ pub fn plot_values<P: AsRef<Path>>(
     label: &str,
     save_path: P,
 ) -> Result<()> {
-    let trace1 = Scatter::new(vec![1, 2, 3, 4], vec![10, 15, 13, 17])
-        .name("trace1")
-        .mode(Mode::Markers);
-    let trace2 = Scatter::new(vec![2, 3, 4, 5], vec![16, 5, 11, 9])
-        .name("trace2")
+    let trace1 = Scatter::new(epochs_seen.clone(), train_values)
+        .name(format!("Training {label}").as_str())
+        .mode(Mode::LinesMarkers);
+    let trace2 = Scatter::new(epochs_seen, val_values)
+        .name(format!("Validation {label}").as_str())
         .mode(Mode::Lines);
-    let trace3 = Scatter::new(vec![1, 2, 3, 4], vec![12, 9, 15, 12]).name("trace3");
 
+    let layout = Layout::new().title(format!("Classification {label}").as_str());
     let mut plot = Plot::new();
     plot.add_trace(trace1);
     plot.add_trace(trace2);
-    plot.add_trace(trace3);
+    plot.set_layout(layout);
     plot.write_html(save_path);
     Ok(())
 }
