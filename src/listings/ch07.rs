3fbcabe73b875a29000eb287d8cec85cf86938b4
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2024-12-30 16:39:25 -0500

    Listing 7.1 (#200)
    * start ch07 listings mod;  struct
    
    * add serde dep
    
    * add serde_json dep
    
    * impl download and load into json str
    
    * impl deserialize into InstructionResponseExample
    
    * impl download_and_load_file

---
+++ src/listings/ch07.rs
@@ -0,0 +1,74 @@
+//! Listings from Chapter 7
+
+use anyhow::Context;
+use bytes::Bytes;
+use serde::{Deserialize, Serialize};
+use std::{
+    fmt::Display,
+    fs::{read_to_string, File},
+    io,
+    path::Path,
+};
+
+#[allow(dead_code)]
+pub const INSTRUCTION_DATA_FILENAME: &str = "instruction_data.json";
+#[allow(dead_code)]
+pub const DATA_DIR: &str = "data";
+pub const INSTRUCTION_DATA_URL: &str = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch\
+/main/ch07/01_main-chapter-code/instruction-data.json";
+
+/// A type for containing an instruction-response pair
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct InstructionResponseExample {
+    instruction: String,
+    input: Option<String>,
+    output: String,
+}
+
+impl Display for InstructionResponseExample {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Instruction: {}\nInput: {:?}\nOutput: {}",
+            self.instruction, self.input, self.output
+        )
+    }
+}
+
+/// [Listing 7.1] Downloading the dataset
+#[allow(unused_variables)]
+pub fn download_and_load_file<P: AsRef<Path>>(
+    file_path: P,
+    url: &str,
+    overwrite: bool,
+) -> anyhow::Result<Vec<InstructionResponseExample>> {
+    if !file_path.as_ref().exists() || overwrite {
+        // download json file
+        let resp = reqwest::blocking::get(url)?;
+        let content: Bytes = resp.bytes()?;
+        let mut out = File::create(file_path.as_ref())?;
+        io::copy(&mut content.as_ref(), &mut out)?;
+    }
+    let json_str = read_to_string(file_path.as_ref())
+        .with_context(|| format!("Unable to read {}", file_path.as_ref().display()))?;
+    let data: Vec<InstructionResponseExample> = serde_json::from_str(&json_str[..])?;
+
+    Ok(data)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+    use rstest::*;
+    use tempfile::NamedTempFile;
+
+    #[rstest]
+    fn test_download_and_load_file() -> Result<()> {
+        let test_file = NamedTempFile::new().unwrap();
+        let file_path = test_file.into_temp_path().keep().unwrap();
+        let data = download_and_load_file(file_path, INSTRUCTION_DATA_URL, true)?;
+        assert_eq!(data.len(), 1100);
+        Ok(())
+    }
+}
