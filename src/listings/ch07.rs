9fd3e3eb9ca4547c4ccd3d1762b293430bc1fe79
nerdai <andrei@nerdai.io>
2024-12-30 15:34:18 -0500

    start ch07 listings mod;  struct

---
+++ src/listings/ch07.rs
@@ -0,0 +1,29 @@
+//! Listings from Chapter 7
+
+use std::{fmt::Display, path::Path};
+
+#[derive(Debug, Default)]
+pub struct InstructionResponseExample {
+    instruction: String,
+    input: Option<String>,
+    output: String,
+}
+
+impl Display for InstructionResponseExample {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Instruction: {}\nInput: {:?}\nOutput: {}",
+            self.instruction, self.input, self.output
+        )
+    }
+}
+
+/// [Listing 7.1] Downloading the dataset
+#[allow(unused_variables)]
+pub fn download_and_load_file<P: AsRef<Path>>(
+    file_path: P,
+    url: &str,
+) -> anyhow::Result<Vec<InstructionResponseExample>> {
+    todo!()
+}
