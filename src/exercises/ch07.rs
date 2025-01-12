bc9fad6d1cec8446c91e7069b520e1c09be71014
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2025-01-12 11:20:25 -0500

    Exercise 7.3 (#252)
    * scaffolding
    
    * add to registry, and download alpaca file
    
    * ex 7.3
    
    * reduce batch size to 2 to avoid OOM
    
    * modify allowed max length to avoid OOM

--- src/exercises/ch07.rs
+++ src/exercises/ch07.rs
@@ -336,6 +336,190 @@ impl Exercise for X2 {
     }
 }
 
+/// # Changing prompt styles
+///
+/// #### Id
+/// 7.3 Fine-tuning on the original Alpaca dataset
+///
+/// #### CLI command
+/// ```sh
+/// # without cuda
+/// cargo run exercise 7.3
+///
+/// # with cuda
+/// cargo run --features cuda exercise 7.3
+/// ```
+pub struct X3;
+
+impl X3 {
+    pub fn get_data_loaders<P: crate::listings::ch07::PromptFormatter>(
+        &self,
+        prompt_formatter: &P,
+        batch_size: usize,
+        allowed_max_length: Option<usize>,
+        verbose: bool,
+    ) -> Result<(
+        crate::listings::ch07::InstructionDataLoader<
+            crate::listings::ch07::InstructionDataCollator,
+        >,
+        crate::listings::ch07::InstructionDataLoader<
+            crate::listings::ch07::InstructionDataCollator,
+        >,
+        crate::listings::ch07::InstructionDataLoader<
+            crate::listings::ch07::InstructionDataCollator,
+        >,
+    )> {
+        use crate::listings::ch07::{
+            download_and_load_file, partition_data, DataLoader, InstructionDataCollator,
+            InstructionDataLoader, InstructionDataset, DATA_DIR,
+        };
+        use candle_core::Device;
+        use std::path::Path;
+        use tiktoken_rs::get_bpe_from_model;
+
+        let tokenizer = get_bpe_from_model("gpt2")?;
+
+        // load instruction examples
+        let file_name = "alpaca_data.json";
+        let alpaca_url =
+            "https://raw.githubusercontent.com/tatsu-lab/stanford_alpaca/main/alpaca_data.json";
+        let file_path = Path::new(DATA_DIR).join(file_name);
+        let data = download_and_load_file(file_path, alpaca_url, false)?;
+
+        // partition data and create train, val, test datasets
+        let (train_data, val_data, test_data) = partition_data(data, 0.85_f32, 0.05_f32)?;
+        let train_dataset = InstructionDataset::new(train_data, &tokenizer, prompt_formatter);
+        let val_dataset = InstructionDataset::new(val_data, &tokenizer, prompt_formatter);
+        let test_dataset = InstructionDataset::new(test_data, &tokenizer, prompt_formatter);
+
+        // create loaders
+        let collator = InstructionDataCollator::new()
+            .device(Device::cuda_if_available(0)?)
+            .allowed_max_length(allowed_max_length);
+        let train_loader =
+            InstructionDataLoader::new(train_dataset, batch_size, true, true, collator.clone());
+        let val_loader =
+            InstructionDataLoader::new(val_dataset, batch_size, false, false, collator.clone());
+        let test_loader =
+            InstructionDataLoader::new(test_dataset, batch_size, false, false, collator);
+
+        if verbose {
+            println!("Train loader:");
+            let mut batcher = train_loader.batcher();
+            while let Some(Ok((inputs, targets))) = batcher.next() {
+                println!("inputs: {:?} targets: {:?}", inputs, targets);
+            }
+        }
+
+        Ok((train_loader, val_loader, test_loader))
+    }
+}
+
+impl Exercise for X3 {
+    fn name(&self) -> String {
+        "7.3".to_string()
+    }
+
+    fn title(&self) -> String {
+        "Fine-tuning on the original Alpaca dataset".to_string()
+    }
+
+    fn statement(&self) -> String {
+        let stmt = "The Alpaca dataset, by researchers at Stanford, is one \
+        of the earliest and most popular openly shared instruction datasets, \
+        consisting of 52,002 entries. As an alternative to the \
+        `instruction-data.json` file we use here, consider fine-tuning an LLM on \
+        this dataset. The dataset is available at https://mng.bz/NBnE. \
+        \n\n\
+        This dataset contains 52,002 entries, which is approximately 50 times \
+        more than those we used here, and most entries are longer. Thus, I \
+        highly recommend using a GPU to conduct the training, which will \
+        accelerate the fine-tuning process. If you encounter out-of-memory \
+        errors, consider reducing the batch_size from 8 to 4, 2, or even 1. \
+        Lowering the allowed_max_length from 1,024 to 512 or 256 can also \
+        help manage memory problems";
+        stmt.to_string()
+    }
+
+    fn main(&self) -> Result<()> {
+        use crate::listings::{
+            ch04::Config,
+            ch05::plot_losses,
+            ch07::{
+                download_and_load_gpt2, train_model_simple, AlpacaPromptFormatter, PromptFormatter,
+                DEFAULT_IGNORE_INDEX,
+            },
+        };
+        use candle_core::{DType, Device};
+        use candle_nn::{AdamW, Optimizer, ParamsAdamW, VarBuilder, VarMap};
+        use ndarray::linspace;
+        use std::path::Path;
+        use tiktoken_rs::get_bpe_from_model;
+
+        // use `download_and_load_gpt2`
+        let model_id = "openai-community/gpt2";
+        let mut cfg = Config::gpt2_124m();
+        cfg.qkv_bias = true;
+        let varmap = VarMap::new();
+        let vb = VarBuilder::from_varmap(&varmap, DType::F32, &Device::cuda_if_available(0)?);
+        let model = download_and_load_gpt2(&varmap, vb.pp("model"), cfg, model_id)?;
+
+        // get data loaders that is built on a dataset that used phi3 prompt format style
+        let prompt_formatter = AlpacaPromptFormatter;
+        let batch_size = 2_usize;
+        let (train_loader, val_loader, _test_loader) =
+            self.get_data_loaders(&prompt_formatter, batch_size, Some(256_usize), false)?;
+
+        // invoke training
+        let (eval_freq, eval_iter, num_epochs) = (5_usize, 5_usize, 1_usize);
+        let optimizer = AdamW::new(
+            varmap.all_vars(),
+            ParamsAdamW {
+                lr: 0.00005,
+                weight_decay: 0.1,
+                ..Default::default()
+            },
+        )?;
+        let tokenizer = get_bpe_from_model("gpt2")?;
+        let start_context = prompt_formatter.format_input(&val_loader.dataset().data()[0]);
+        let (train_losses, val_losses, tokens_seen) = train_model_simple(
+            &model,
+            &train_loader,
+            &val_loader,
+            optimizer,
+            vb.device(),
+            num_epochs,
+            eval_freq,
+            eval_iter,
+            start_context.as_str(),
+            &tokenizer,
+            Some(DEFAULT_IGNORE_INDEX),
+        )?;
+
+        // save model
+        println!("Saving weights to `./ift.alpaca_dataset.checkpoint.safetensors`");
+        varmap.save("ift.alpaca_dataset.checkpoint.safetensors")?;
+
+        // plot loss curves
+        println!("Saving plot to `./plot_ift_alpaca_dataset_loss.html`");
+        let epochs_seen = Vec::from_iter(linspace(0_f32, num_epochs as f32, train_losses.len()));
+        let tokens_seen = tokens_seen
+            .into_iter()
+            .map(|el| el as f32)
+            .collect::<Vec<_>>();
+        let save_path = Path::new("plot_ift_alpaca_dataset_loss.html").to_path_buf();
+        plot_losses(
+            epochs_seen,
+            tokens_seen,
+            train_losses,
+            val_losses,
+            save_path,
+        )?;
+
+        Ok(())
+    }
+}
+
 pub mod addons {
     //! Auxiliary module for exercises::ch07
     use crate::listings::ch07::{
