f1ace48813cb32be4f556bbae46cc4eda9292132
nerdai <andrei@nerdai.io>
2025-01-26 23:15:26 -0500

    impl generate_chosen_and_rejected_response

--- src/listings/ch07/bonus.rs
+++ src/listings/ch07/bonus.rs
@@ -1,6 +1,7 @@
 //! Bonus material module for Chapter 7
 
-use super::{InstructionResponseExample, PromptFormatter};
+use super::{query_model, InstructionResponseExample, PromptFormatter};
+use rand::{rngs::StdRng, Rng, SeedableRng};
 
 #[allow(dead_code)]
 #[derive(Clone, Debug, Default)]
@@ -36,10 +37,36 @@ impl PreferenceExample {
 /// Using Ollama to generate a `chosen` and `rejected` responses for an instruction entry
 #[allow(unused_variables)]
 pub fn generate_chosen_and_rejected_response<P: PromptFormatter>(
-    instruction_data: &[InstructionResponseExample],
+    entry: &InstructionResponseExample,
     url: &str,
     model: &str,
     prompt_formatter: &P,
 ) -> anyhow::Result<PreferenceExample> {
-    todo!()
+    let mut rng = StdRng::seed_from_u64(69420);
+    let u: f32 = rng.gen_range(0.0..1.0);
+    let politeness = if u < 0.5 { "polite" } else { "impolite" };
+
+    let prompt = format!(
+        "Given the input `{}` and correct output `{}`, \
+        slightly rewrite the output to be more {}
+        Keep the modification minimal.
+        Only return return the generated response and nothing else.",
+        prompt_formatter.format_input(entry),
+        entry.output(),
+        politeness
+    );
+    println!("{}", prompt);
+
+    let response = query_model(prompt.as_str(), model, url)?;
+    let mut preference_example = PreferenceExample::from(entry.clone());
+
+    if politeness == "polite" {
+        preference_example.set_chosen(response.as_str());
+        preference_example.set_rejected(entry.output().as_str());
+    } else {
+        preference_example.set_chosen(entry.output().as_str());
+        preference_example.set_rejected(response.as_str());
+    }
+
+    Ok(preference_example)
 }
