132916b32dda8d3286e7bd8a0bee331f19aaeaba
nerdai <andrei@nerdai.io>
2025-01-26 20:53:00 -0500

    Preference Example

--- src/listings/ch07/bonus.rs
+++ src/listings/ch07/bonus.rs
@@ -1,1 +1,35 @@
 //! Bonus material module for Chapter 7
+
+use super::{InstructionResponseExample, PromptFormatter};
+
+#[allow(dead_code)]
+#[derive(Clone, Debug, Default)]
+pub struct PreferenceExample {
+    instruction: String,
+    input: Option<String>,
+    output: String,
+    rejected: String,
+    chosen: String,
+}
+
+impl From<InstructionResponseExample> for PreferenceExample {
+    fn from(value: InstructionResponseExample) -> Self {
+        Self {
+            instruction: value.instruction().to_owned(),
+            input: value.input().to_owned(),
+            output: value.output().to_owned(),
+            ..Default::default()
+        }
+    }
+}
+
+/// Using Ollama to generate a `chosen` and `rejected` responses for an instruction entry
+#[allow(unused_variables)]
+pub fn generate_chosen_and_rejected_response<P: PromptFormatter>(
+    instruction_data: &[InstructionResponseExample],
+    url: &str,
+    model: &str,
+    prompt_formatter: &P,
+) -> anyhow::Result<PreferenceExample> {
+    todo!()
+}
