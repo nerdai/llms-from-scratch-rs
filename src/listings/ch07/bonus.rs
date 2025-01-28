2222f5fd402e5ab6fa9bd70e1d033804141b717f
nerdai <andrei@nerdai.io>
2025-01-28 00:48:13 -0500

    initial implementation of generate_preference_dataset

--- src/listings/ch07/bonus.rs
+++ src/listings/ch07/bonus.rs
@@ -1,12 +1,23 @@
 //! Bonus material module for Chapter 7
 
 use super::{query_model, InstructionResponseExample, PromptFormatter};
+use anyhow::Context;
 use rand::{rngs::StdRng, Rng, SeedableRng};
+use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, NoneAsEmptyString};
+use std::{
+    fs::{read_to_string, File},
+    io,
+    io::Write,
+    path::Path,
+};
+use tqdm::tqdm;
 
-#[allow(dead_code)]
-#[derive(Clone, Debug, Default)]
+#[serde_as]
+#[derive(Debug, Default, Serialize, Deserialize, Clone, PartialEq)]
 pub struct PreferenceExample {
     instruction: String,
+    #[serde_as(as = "NoneAsEmptyString")]
     input: Option<String>,
     output: String,
     rejected: String,
@@ -35,7 +46,6 @@ impl PreferenceExample {
 }
 
 /// Using Ollama to generate a `chosen` and `rejected` responses for an instruction entry
-#[allow(unused_variables)]
 pub fn generate_chosen_and_rejected_response<P: PromptFormatter>(
     entry: &InstructionResponseExample,
     url: &str,
@@ -68,6 +78,52 @@ pub fn generate_chosen_and_rejected_response<P: PromptFormatter>(
     }
 
     Ok(preference_example)
+}
+
+/// Helper function to write instruction data to a json
+pub fn load_preference_data_from_json<P: AsRef<Path>, T: Serialize + for<'a> Deserialize<'a>>(
+    file_path: P,
+) -> anyhow::Result<Vec<T>> {
+    let json_str = read_to_string(file_path.as_ref())
+        .with_context(|| format!("Unable to read {}", file_path.as_ref().display()))?;
+    let data: Vec<T> = serde_json::from_str(&json_str[..])?;
+    Ok(data)
+}
+
+/// Helper function to write instruction data to a json
+pub fn write_preference_data_to_json<P: AsRef<Path>, S: Serialize + for<'a> Deserialize<'a>>(
+    instruction_data: &Vec<S>,
+    save_path: P,
+) -> anyhow::Result<()> {
+    let file = File::create(save_path)?;
+    let mut writer = io::BufWriter::new(file);
+    serde_json::to_writer(&mut writer, instruction_data)?;
+    writer.flush()?;
+    Ok(())
+}
+
+pub fn generate_preference_dataset<P: PromptFormatter, T: AsRef<Path>>(
+    instruction_data: &[InstructionResponseExample],
+    url: &str,
+    model: &str,
+    prompt_formatter: &P,
+    save_path: T,
+) -> anyhow::Result<()> {
+    let mut dataset = vec![];
+    for entry in tqdm(instruction_data.iter()) {
+        let preference_example =
+            generate_chosen_and_rejected_response(entry, url, model, prompt_formatter)?;
+        dataset.push(preference_example);
+    }
+
+    // write to json
+    println!(
+        "Saving preference data to {:?}",
+        save_path.as_ref().to_str()
+    );
+    write_preference_data_to_json(&dataset, save_path)?;
+
+    Ok(())
 }
 
 #[cfg(test)]
